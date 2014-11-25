dade09be9cc120c59cfc467cc29df0261727f4b4
Ernesto Medina <ernezto@gmail.com>
2014-11-25 15:05:17 -0500

    [#307] - con @sriprasanna - Implementando serializacion para DocumentoFirma

--- src/main/java/ec/gob/senescyt/firma/core/DocumentoFirmado.java
+++ src/main/java/ec/gob/senescyt/firma/core/DocumentoFirmado.java
@@ -1,10 +1,16 @@
 package ec.gob.senescyt.firma.core;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import ec.gob.senescyt.microservicios.commons.core.EntidadBase;
 
 public class DocumentoFirmado extends EntidadBase {
     private String firmaDigital;
+
+    @JsonIgnore
     private ConfiguracionFirma configuracionFirma;
+
+    private DocumentoFirmado() {
+    }
 
     public DocumentoFirmado(String firmaDigital, ConfiguracionFirma configuracionFirma) {
         this.firmaDigital = firmaDigital;
--- src/test/java/ec/gob/senescyt/firma/core/DocumentoFirmadoTest.java
+++ src/test/java/ec/gob/senescyt/firma/core/DocumentoFirmadoTest.java
@@ -1,10 +1,15 @@
 package ec.gob.senescyt.firma.core;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.apache.commons.lang.RandomStringUtils.randomAlphabetic;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class DocumentoFirmadoTest {
@@ -28,5 +33,14 @@ public class DocumentoFirmadoTest {
     @Test
     public void debeTenerLaConfiguracionDeFirma() {
         assertThat(documentoFirmado.getConfiguracionFirma(), is(configuracionFirma));
+    }
+
+    @Test
+    public void debeSerializarYDeserializarUnDocumento() throws IOException {
+        ObjectMapper mapper = Jackson.newObjectMapper();
+        String entidadSerializada = mapper.writeValueAsString(documentoFirmado);
+        DocumentoFirmado entidadDeserializada = mapper.readValue(entidadSerializada, DocumentoFirmado.class);
+        assertThat(entidadDeserializada.getFirmaDigital(), is(firmaDigital));
+        assertThat(entidadDeserializada.getConfiguracionFirma(), is(nullValue()));
     }
 }
